5cd77876e7ab48c1f89a3cfc1c7d1be458b590a3
milasantacruz <milalovechaton@gmail.com>
2022-04-27 00:22:18 -0300

    firat commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ff316e952e3f102c2379a565beca7333'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6a54ca47-e7f7-4cdf-8d6c-2fcf45e2d64c'
+};
